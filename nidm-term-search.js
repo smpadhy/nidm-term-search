29bea71652d54bb48c4cea0297de2ecaf8ec5d3c
Smruti Padhy <smruti.padhy@gmail.com>
2017-07-10 11:51:45 -0400

    code clean up

--- nidm-term-search.js
+++ nidm-term-search.js
@@ -2,17 +2,7 @@ const path = require('path')
 const fs = require('fs')
 const rdfstore = require('rdfstore')
 
-let query1 = " PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>\
-SELECT *\
-WHERE { ?subject rdfs:label ?object .\
-    } \
-"
-let query2 = " PREFIX obo:<http://purl.obolibrary.org/obo/>\
-SELECT *\
-WHERE { ?subject obo:IAO_0000115 ?object .\
-    } \
-"
-let query3 = " PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>\
+let query = " PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>\
 PREFIX obo:<http://purl.obolibrary.org/obo/>\
 SELECT *\
 WHERE { ?subject rdfs:label ?label ;\
@@ -25,19 +15,10 @@ new rdfstore.Store(function(err, store) {
     if(err){
       console.log(err)
     }
-    console.log("inside store load", results)
-    /*store.execute(query1,function(success,results1) {
-      console.log("after query1~~~~~>: ",results1)
-    })
-
-    store.execute(query2,function(success,results2) {
-      console.log("------Starting Query 2 ----------")
-      console.log("after query 2---->: ",results2)
-    })*/
-    store.execute(query3,function(success,results3) {
-      console.log("------Starting Query 3 ----------")
-      console.log("after query 3---->: ",results3)
+    //console.log("inside store load", results)
+    store.execute(query,function(success,results) {
+      console.log("----- Query Results  ----------")
+      console.log(results)
     })
   })
-
 })
